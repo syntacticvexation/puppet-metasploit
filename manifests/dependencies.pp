4fc49a903a2c18e06e3ac94a5c6582a06be86609
Syntactic Vexation <syntacticvexation@gmail.com>
2016-12-30 11:57:21 +1000

    remove locate, already have it

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -4,7 +4,7 @@
 #
 class metasploit::dependencies {
   $packages = ['build-essential', 'zlib1g', 'zlib1g-dev', 'libxml2',
-    'libxml2-dev', 'libxslt1-dev', 'locate', 'libreadline6-dev',
+    'libxml2-dev', 'libxslt1-dev', 'libreadline6-dev',
     'libcurl4-openssl-dev', 'git-core', 'libssl-dev', 'libyaml-dev',
     'openssl', 'autoconf', 'libtool', 'ncurses-dev', 'bison', 'curl',
     'wget', 'postgresql', 'postgresql-contrib', 'libpq-dev', 'libapr1',
